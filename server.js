adb08f463a461cd9d7ffbc2317d1894f05c76c1c
leftdeadlive81 <leftdeadlive81@gmail.com>
2026-08-02 16:32:23 +0900

    add resource status

--- server.js
+++ server.js
@@ -3,206 +3,149 @@ const app = express();
 
 app.use(express.static("public"));
 
-
 const http = require("http");
-
 const sqlite3 = require("sqlite3").verbose();
-
 
 const db = new sqlite3.Database("gps.db");
 
 
-
 // テーブル作成
-db.serialize(()=>{
+db.serialize(() => {
 
     db.run(`
     CREATE TABLE IF NOT EXISTS locations(
-
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-
         name TEXT,
-
         lat REAL,
-
         lon REAL,
-
-        fuel INTEGER,
-
-        money INTEGER,
-
-        equipment INTEGER,
-
+        money INTEGER DEFAULT 0,
+        fuel INTEGER DEFAULT 0,
+        equipment INTEGER DEFAULT 0,
         time DATETIME DEFAULT CURRENT_TIMESTAMP
-
     )
     `);
 
-
 });
 
 
-
+// HTTPサーバー
 const server = http.createServer(app);
 
 
+// Socket.IO
 const io = require("socket.io")(server);
 
 
-
-let users={};
-
-
-
-
-// 接続
-io.on("connection",socket=>{
-
-
-console.log("接続:",socket.id);
+// 接続中ユーザー
+let users = {};
 
 
 
-// GPS受信
+io.on("connection", socket => {
 
-socket.on("location",data=>{
-
-
-
-users[socket.id]=data;
+    console.log("接続:", socket.id);
 
 
 
-
-db.run(
-
-`
-INSERT INTO locations
-(
-name,
-lat,
-lon,
-fuel,
-money,
-equipment
-)
-
-VALUES(?,?,?,?,?,?)
-
-`,
-
-[
-
-data.name,
-
-data.lat,
-
-data.lon,
-
-data.fuel,
-
-data.money,
-
-data.equipment
+    // GPS情報受信
+    socket.on("location", data => {
 
 
-]
-
-
-);
+        // 現在位置保存
+        users[socket.id] = data;
 
 
 
+        // DB保存
+        db.run(
+        `
+        INSERT INTO locations
+        (
+            name,
+            lat,
+            lon,
+            money,
+            fuel,
+            equipment
+        )
+        VALUES(?,?,?,?,?,?)
+        `,
+        [
+            data.name,
+            data.lat,
+            data.lon,
+            data.money || 0,
+            data.fuel || 0,
+            data.equipment || 0
+        ]);
 
-// 全員へ配信
 
-io.emit(
-"locations",
-users
-);
+
+        // 全員へ送信
+        io.emit("locations", users);
+
+
+    });
+
+
+
+    // 切断
+    socket.on("disconnect", () => {
+
+
+        delete users[socket.id];
+
+
+        io.emit("locations", users);
+
+
+    });
 
 
 
 });
-
-
-
-
-// 切断
-
-socket.on("disconnect",()=>{
-
-
-delete users[socket.id];
-
-
-io.emit(
-"locations",
-users
-);
-
-
-});
-
-
-
-});
-
-
 
 
 
 // 履歴取得
-
-app.get("/history",(req,res)=>{
-
-
-db.all(
-
-`
-SELECT *
-FROM locations
-ORDER BY time
-`,
-
-[],
-
-(err,rows)=>{
+app.get("/history", (req,res)=>{
 
 
-if(err){
-
-res.status(500)
-.send(err.message);
-
-return;
-
-}
-
-
-res.json(rows);
+    db.all(
+        `
+        SELECT *
+        FROM locations
+        ORDER BY time
+        `,
+        [],
+        (err,rows)=>{
 
 
+            if(err){
 
-}
+                res.status(500).send(err.message);
+                return;
+
+            }
 
 
-);
+            res.json(rows);
 
+
+        }
+    );
 
 
 });
 
 
 
+// Render対応
+const PORT = process.env.PORT || 3000;
 
 
-server.listen(3000,()=>{
+server.listen(PORT,()=>{
 
-
-console.log(
-"http server start"
-);
-
+    console.log("http server start port:" + PORT);
 
 });
